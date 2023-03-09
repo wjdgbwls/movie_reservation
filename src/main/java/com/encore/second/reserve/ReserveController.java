46392a2a8dd23715adc6b1bc63cba8a5517aa81c
wjdgbwls <62253315+wjdgbwls@users.noreply.github.com>
2023-03-09 10:00:13 +0900

    Update ReserveController.java

--- src/main/java/com/encore/second/reserve/ReserveController.java
+++ src/main/java/com/encore/second/reserve/ReserveController.java
@@ -40,18 +40,6 @@ public class ReserveController {
 
 
 	
-//	//내가 예약한 영화 목록
-//	@GetMapping("/mylist")
-//	public void mylist(Map map, HttpSession session) {// map: 뷰 페이지에 전달할 값들을 담을라고
-//	// 세션에 "loginid"이름으로 저장된 값을 추출 => 현재 로그인한 사람의 id
-//	String loginid = (String) session.getAttribute("loginid");
-//
-//	// 예약자가 loginid인 예약을 검색
-//	ArrayList<Reserve> list = service.getByUser_id(loginid);
-//	map.put("list", list);// 뷰페이지에 전달할 데이터를 map에 담음
-//	}
-//	
-	
 	@GetMapping("/pay")
 	public String myinfo(String seatList, int id, int anum, int cnum, Map map) {// myinfo.jsp에 로그인 사람의 정보를 출력
 		ArrayList<Seat> list = new ArrayList<>();
@@ -110,106 +98,5 @@ public class ReserveController {
 		// return "/ReservationCheck/check?id=rId";
 		return "redirect:/Home/main";
 	}
-//	@GetMapping("/list")
-//	public void list(Map map) {
-//		//ArrayList<Reserve> list = service.getAll(); getAll 메소드를 만들어서 받아온 예약정보를 객체로 만들어 뿌림
-//		//map.put("list", list)
-//	}
-//------------------------------------------------------------------------------------------------------------------------
-//	@Autowired
-//	CGVReserveService cgvReserveService;
-//	
-//	@Autowired
-//	CGVPayService cgvPayService;
-//	
-//	private static Logger logger = LoggerFactory.getLogger(CGVInfoController.class);
-//	@RequestMapping(value="moveReserve.do", method = {RequestMethod.GET, RequestMethod.POST})
-//	public String moveReserve() {
-//		logger.info("moveReserve");
-//		return "reserve";
-//	}
-//	
-//	
-//	@RequestMapping(value="moveSeat.do", method = {RequestMethod.GET, RequestMethod.POST})
-//	public String moveSeat(Model model, CGVReserveDto dto) {
-//		
-//		System.out.println(dto.toString());
-//		logger.info("moveSeat");
-//		model.addAttribute("reserve", dto);
-//		
-//		return "seat";
-//	}
-//	
-//
-//	@RequestMapping(value="moveKakao.do", method = {RequestMethod.GET, RequestMethod.POST})
-//	public String moveKakao(Model model, CGVReserveDto dto, CGVPayDto payDto, HttpSession session) {	
-//		System.out.println(dto.toString());
-//		logger.info("moveKakao");
-//		CGVMemberDto login =(CGVMemberDto)session.getAttribute("login");
-//		
-//		dto.setId(login.getId());
-//		boolean isSuccess = cgvReserveService.CGVReserve(dto);
-//		System.out.println(isSuccess);
-//		model.addAttribute("reserve", dto);
-//		model.addAttribute("pay", payDto);
-//		if(isSuccess == false) {
-//			System.out.println("오류가 났어요...");
-//			return "redirect:/moveMain.do";
-//		}
-//		return "kakao";
-//	}
-//	
-//	@RequestMapping(value="payKakao.do", method = {RequestMethod.GET, RequestMethod.POST})
-//	public String payKakao(Model model, CGVReserveDto dto, CGVPayDto payDto, HttpSession session) {	
-//		
-//		logger.info("moveKakao");
-//		CGVMemberDto login =(CGVMemberDto)session.getAttribute("login");
-//	
-//		 dto.setId(login.getId());
-//		 System.out.println(dto.toString() +"이것이 dto다");
-//		 List<CGVReserveDto> list = cgvReserveService.getCGVReserve(dto);
-//		/* CGVReserveDto cgvReserveDto = cgvReserveService.getCGVReserve(dto); */
-////		if(cgvReserveDto != null) {
-//			System.out.println("여기있다");
-////			System.out.println(cgvReserveDto.toString());
-////		}
-//		payDto.setId(list.get(0).getId());
-//		payDto.setReserveSequence(list.get(0).getReserveSequence());
-//		System.out.println("문제찾기");
-//		System.out.println(payDto.toString());
-//		boolean isSuccess = cgvPayService.payTicket(payDto);
-//		System.out.println(isSuccess);
-//		if(isSuccess == false) {
-//			System.out.println("오류가 났어요...");
-//			return "redirect:/moveMain.do";
-//		}
-//		model.addAttribute("type", "reserve");
-//		model.addAttribute("isSuccess", isSuccess);
-//		/*
-//		 * model.addAttribute("reserve", dto); model.addAttribute("pay", payDto);
-//		 */
-//		
-//		return "process";
-//	}
-//	
-//	@RequestMapping(value="moveMypage.do", method = {RequestMethod.GET, RequestMethod.POST})
-//	public String moveMain(Model model, CGVReserveDto reserveDto, CGVPayDto payDto, HttpSession session) {
-//		CGVMemberDto login =(CGVMemberDto)session.getAttribute("login");
-//		
-//		 
-//		reserveDto.setId(login.getId());
-//		List<CGVReserveDto> list = cgvReserveService.getPayJoinTable(reserveDto);
-//		
-//		if(list != null) {
-//			for(CGVReserveDto dto : list) {
-//				System.out.println(dto.toString());
-//				model.addAttribute("reserveList", list);
-//			}
-//		}else {
-//			System.out.println("이것이 오류다");
-//		}
-//		
-//		return "myPage";
-//	}
-//}
+
 }
